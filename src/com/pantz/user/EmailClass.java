072a320920eee0c5ece97b176af5ec81fcc2e4ed
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2021-02-04 13:28:40 +0200

    Commit with EmailClass is Ok (I think)

--- src/com/pantz/user/EmailClass.java
+++ src/com/pantz/user/EmailClass.java
@@ -3,7 +3,7 @@ package com.pantz.user;
 public class EmailClass {
 
    private int id;
-   private  boolean isNew;
+   private  boolean isNewEmail;
    private String sender;
    private String receiver;
    private String subject;
@@ -18,16 +18,73 @@ public class EmailClass {
         this.mainbody = mainbody;
 
         this.id = id;
-        this.isNew = true;
+        this.isNewEmail = true;
         this.idIncreaser=1; //μπορει να θελει αλλαγη θεσης
     }
 
 
+    /**
+     * We have to increase the email id in order to
+     * have unique ids for our emails.
+     * @param id --> For each mail sets a unique id
+     */
     public void setId(int id) {
         id = idIncreaser++;
     }
 
+
+ /*   public boolean isNewEmail() {
+        return isNewEmail;
+    }*/
+
+    ///////////////////////////////////////SETTERS-GETTERS///////////////////////////////////////////
+
+
+
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMainbody() {
+        return mainbody;
+    }
+
+    public int getIdIncreaser() {
+        return idIncreaser;
+    }
+
     public int getId() {
         return id;
+    }
+
+
+    /**
+     * Checks if the email is old mail
+     * and sets the @variable isNewEmail= false
+     */
+    public void isRead(){
+
+        isNewEmail =false;
+
+    }
+
+
+    /**
+     * Checks if the email is new mail
+     * and sets the @variable isNewEmail= true
+     */
+
+
+    public void isNew(){
+        isNewEmail = true;
     }
 }
